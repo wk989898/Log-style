ae7b9ebd98d363ffc2534d1322da55bb555207cd
wk989898 <wangkun989898@gmail.com>
2021-03-15 21:08:24 +0800

    use log title

--- log.js
+++ log.js
@@ -1,52 +1,53 @@
 /**
+ * @param {boolean} logTitle if true,it will try find a style tag which `title` is `log`. default: false
+ * @returns {Function}
  * @author wk989898
  * @license MIT
  */
-if (log === void 0)
-  var log = (function () {
-    /**
-     * @param {string} text 
-     * @returns {number} 
-     */
-    function getLen(text) {
-      var words = text.split('%c').slice(1)
-      return words.length
-    }
+function Logstyle(logTitle = false) {
+  /**
+   * @param {string} text 
+   * @returns {number} 
+   */
+  function getLen(text) {
+    var words = text.split('%c').slice(1)
+    return words.length
+  }
 
-    /**
-    * @param {string} text 
-    * @param {Array<string>} css
-    */
-    function log(text, ...css) {
-      if (css.length == 1)
-        css = css[0].split(' ')
-      var textLen = getLen(text)
-      var cssLen = css.length
-      var style = css
-      if (cssLen > textLen) {
-        var isConvert = new Array(cssLen - textLen).fill(false)
-        let i = cssLen - textLen
-        while (i < cssLen) {
-          if (/^%css/.test(css[i])) {
-            style[i] = css[i].replace(/^%css/, '')
-            isConvert[i - textLen] = true
-          }
-          i++
+  /**
+  * @param {string} text 
+  * @param {Array<string>} css
+  */
+  return function (text, ...css) {
+    if (css.length == 1)
+      css = css[0].split(' ')
+    var textLen = getLen(text)
+    var cssLen = css.length
+    var style = css
+    if (cssLen > textLen) {
+      var isConvert = new Array(cssLen - textLen).fill(false)
+      let i = cssLen - textLen
+      while (i < cssLen) {
+        if (/^%css/.test(css[i])) {
+          style[i] = css[i].replace(/^%css/, '')
+          isConvert[i - textLen] = true
         }
+        i++
       }
-      var styleSheets = document.styleSheets
-      Object.values(styleSheets).forEach(s => {
+    }
+    var styleSheets = document.styleSheets
+    Object.values(styleSheets).forEach(s => {
+      if (logTitle && s.title == 'log'
+        || !logTitle)
         Object.values(s.cssRules).forEach(t => {
           var index = css.indexOf(t.selectorText)
           if (index != -1 && (index < textLen || isConvert[index - textLen])) {
             style[index] = t.cssText.replace(/^.*\{(.*)\}/, '$1')
           }
         })
-      })
-
-      return console.log(text, ...style)
-    }
-    return log
-  })();
+    })
+    return console.log(text, ...style)
+  }
+};
 
 
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ use css to make `console.log` colourful
 
 ## Use
 ``` html
-<style>
+<style title="log"> //if logTitle is true. It applies only to this tag
     #app{
       color: red;
       font-size: 15px;
@@ -11,8 +11,15 @@ use css to make `console.log` colourful
     .mid{
       color: burlywood;
     }
-  </style> 
+</style> 
+<style>
+    .main {
+      color: blue;
+      font-size: 40px;
+    }
+</style>
 <script>
+    var log = Logstyle(false)
     console.log('%capp%cmid', 'color: red;font-size: 15px;', 'color: burlywood;font-size: 30px;')
     log('%capp%cmid', '#app', '.mid')
     log('%capp', '#app', '.mid')
@@ -21,5 +28,6 @@ use css to make `console.log` colourful
     log('%cmain%capp', '.main #app')
 </script>
 ```
+
 ### Screenshoot  
 ![image](https://user-images.githubusercontent.com/55834428/110735324-8c78ce80-8264-11eb-9480-fab77b4d106f.png)
